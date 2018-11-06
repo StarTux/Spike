f9843d03b73e14e6443cfe8fb9f89af2bc6df445
StarTux <ths.loy@googlemail.com>
2018-11-06 12:42:57 +0100

    Clean up the code and write some useful commands

--- src/main/java/com/winthier/spike/SpikePlugin.java
+++ src/main/java/com/winthier/spike/SpikePlugin.java
@@ -1,39 +1,71 @@
 package com.winthier.spike;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.Plugin;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public final class SpikePlugin extends JavaPlugin {
-    final TickTask tickTask;
-    final WatchTask watchTask;
+    private WatchTask watchTask = new WatchTask(this);
 
-    public SpikePlugin() {
-        this.tickTask = new TickTask(this);
-        this.watchTask = new WatchTask(this);
-    }
+    // --- JavaPlugin
 
     @Override
     public void onEnable() {
-        this.tickTask.enable();
+        saveDefaultConfig();
+        importConfig();
         this.watchTask.enable();
-        new BukkitRunnable() {
-            public void run() {
-                SpikePlugin.this.watchTask.mainThread = Thread.currentThread();
-            }
-        }.runTaskLater((Plugin)this, 20L);
+        getServer().getScheduler().runTaskTimer(this, () -> this.watchTask.tick(), 0L, 1L);
+        getServer().getScheduler().runTaskAsynchronously(this, this.watchTask);
     }
 
     @Override
     public void onDisable() {
-        this.watchTask.disable();
-        this.tickTask.disable();
+        this.watchTask.stop();
     }
 
+    // --- Util
+
+    private void importConfig() {
+        reloadConfig();
+        this.watchTask.setReportingThreshold(getConfig().getInt("ReportingThreshold"));
+    }
+
+    // --- Command
+
     @Override
-    public boolean onCommand(final CommandSender commandSender, final Command command, final String s, final String[] array) {
-        return false;
+    public boolean onCommand(final CommandSender sender, final Command command, final String alias, final String[] args) {
+        if (args.length == 0) return false;
+        try {
+            return onCommand(sender, args);
+        } catch (Exception e) {
+            if (sender instanceof HumanEntity) {
+                sender.sendMessage(ChatColor.RED + e.getMessage());
+            } else {
+                e.printStackTrace();
+            }
+            return true;
+        }
+    }
+
+    private boolean onCommand(final CommandSender sender, final String[] args) throws Exception {
+        switch (args[0]) {
+        case "reload": {
+            if (args.length != 1) return false;
+            importConfig();
+            sender.sendMessage("[Spike] Configuration reloaded.");
+            return true;
+        }
+        case "generate": {
+            if (args.length != 2) return false;
+            int ticks = Integer.parseInt(args[1]);
+            sender.sendMessage("[Spike] Generating " + ticks + " missed ticks...");
+            Thread.sleep((long)ticks * 50);
+            sender.sendMessage("[Spike] Generated " + ticks + " missed ticks.");
+            return true;
+        }
+        default: return false;
+        }
     }
 }
--- src/main/java/com/winthier/spike/WatchTask.java
+++ src/main/java/com/winthier/spike/WatchTask.java
@@ -3,98 +3,106 @@ package com.winthier.spike;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.scheduler.BukkitRunnable;
+import java.util.concurrent.atomic.AtomicBoolean;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
-final class WatchTask extends BukkitRunnable {
+/**
+ * A single instance of this class will run in an async task and take
+ * note of missed ticks.
+ */
+@RequiredArgsConstructor
+final class WatchTask implements Runnable {
+    // Const
     private static final int MISSED_TICKS_THRESHOLD = 9;
     private static final int TICKS_PER_LOOP = 3;
-    // Const
+    // Required
     private final SpikePlugin plugin;
+    // Config
+    @Setter private int reportingThreshold = 5;
     // Async
-    private volatile int missedTicks;
-    private volatile boolean cancelled;
+    private volatile AtomicBoolean ticked = new AtomicBoolean(true);
+    private volatile boolean cancelled = false;
     Thread mainThread;
     // Stats
-    private int previouslyMissedTicks;
-    private final Report fullReport;
-    private final Report shortReport;
+    private int missedTicks = 0;
+    private final Report fullReport = new Report();
+    private final Report shortReport = new Report();
     // IO
     private PrintStream out;
-    private final DateFormat dateFormat;
+    private final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-    WatchTask(final SpikePlugin plugin) {
-        this.cancelled = false;
-        this.previouslyMissedTicks = 0;
-        this.dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        this.fullReport = new Report();
-        this.shortReport = new Report();
-        this.plugin = plugin;
-    }
-
+    /**
+     * Set up logging and initialize some state information.  This is
+     * called from the main thread.
+     */
     void enable() {
         this.mainThread = Thread.currentThread();
         this.missedTicks = 0;
         try {
             final File dataFolder = this.plugin.getDataFolder();
             dataFolder.mkdirs();
-            final File file = new File(dataFolder, new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date()) + ".txt");
+            String filename = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date()) + ".log";
+            final File file = new File(dataFolder, filename);
             this.plugin.getLogger().info("Using log file " + file.getPath());
+            // Create PrintStream
             this.out = new PrintStream(file);
             this.out.println(new Date().toString());
             this.out.println();
             this.out.flush();
+            // Create symlink
+            final File link = new File(dataFolder, "latest.log");
+            if (link.exists()) link.delete();
+            Files.createSymbolicLink(link.toPath(), Paths.get(filename));
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        this.runTaskAsynchronously((Plugin)this.plugin);
     }
 
-    void disable() {
+    void stop() {
         this.cancelled = true;
-        try {
-            this.cancel();
-        } catch (IllegalStateException ex) {
-            ex.printStackTrace();
-        }
+        this.ticked.set(true);
     }
 
+    /**
+     * Called by a plugin task once every (main thread) tick.
+     */
     void tick() {
-        this.missedTicks = 0;
+        this.ticked.set(true);
     }
 
     private void loop() {
         try {
-            Thread.sleep(150L);
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
-            return;
+            Thread.sleep(50L);
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
         }
-        if (this.out == null) {
-            return;
-        }
-        final int preMissedTicks = this.missedTicks;
-        this.missedTicks += 3;
-        if (preMissedTicks == 0) {
-            if (this.previouslyMissedTicks > 0) {
-                if (this.previouslyMissedTicks > 9) {
-                    System.out.println(String.format("[Spike] Reporting %d missed ticks", this.previouslyMissedTicks));
-                    this.out.format("%s SPIKE missed %d ticks.\n", this.dateFormat.format(new Date()), this.previouslyMissedTicks);
-                    this.shortReport.report(this.out);
-                    this.out.println();
-                }
-                this.shortReport.reset();
-                this.previouslyMissedTicks = 0;
+        if (this.ticked.getAndSet(false)) {
+            // If we were ticked and the previous no-tick span was
+            // larger than the threshold, log the short report to
+            // console and the logs and reset it.
+            final int missed = this.missedTicks + 1;
+            if (missed >= this.reportingThreshold) {
+                this.plugin.getLogger().info(String.format("Reporting %d missed ticks", missed));
+                this.out.format("%s SPIKE missed %d ticks.\n", this.dateFormat.format(new Date()), missed);
+                this.shortReport.report(this.out);
+                this.out.println();
             }
+            this.missedTicks = 0;
+            this.shortReport.reset();
         } else {
-            for (final StackTraceElement stackTraceElement : this.mainThread.getStackTrace()) {
+            // If we missed a tick, we add this info to the short and
+            // full report.
+            this.missedTicks += 1;
+            for (final StackTraceElement stackTraceElement: this.mainThread.getStackTrace()) {
                 this.fullReport.report(stackTraceElement);
                 this.shortReport.report(stackTraceElement);
             }
-            this.previouslyMissedTicks = preMissedTicks + 3;
         }
     }
 
@@ -104,7 +112,7 @@ final class WatchTask extends BukkitRunnable {
         while (!this.cancelled) {
             this.loop();
         }
-        this.out.format("Final report:\n", new Object[0]);
+        this.out.println("Final report:");
         this.fullReport.report(this.out);
         this.fullReport.reset();
         this.out.close();
